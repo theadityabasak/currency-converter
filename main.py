6cc3bbcae3e744646f28d61571c1b899812b29fc
theadityabasak <orandom46@gmail.com>
2025-10-27 08:11:27 +0000

    Added basic error handling

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import requests
 
 def get_live_rate(base, target):
-    url = f"https://api.frankfurter.app/latest?from={base}&to={target}"
-    data = requests.get(url).json()
-    return data['rates'][target]
+    try:
+        url = f"https://api.frankfurter.app/latest?from={base}&to={target}"
+        data = requests.get(url).json()
+        return data['rates'][target]
+    except Exception:
+        print("Something went wrong. Please check your input or internet connection.")
+        return None
 
-base = input("Enter base currency ")
-target = input("Enter target currency ")
+base = input("Enter base currency: ").upper()
+target = input("Enter target currency: ").upper()
+
 rate = get_live_rate(base, target)
-print(f"1 {base} = {rate} {target}")
+if rate:
+    print(f"1 {base} = {rate} {target}")
